d0d137ec16b2dcf372b9951410a12c15baea949b
Dag Christoffersen <deejunit@gmail.com>
2012-02-01 10:00:23 +0100

    small changes that I forgot

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,10 @@ module Vegskade
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Custom config by rubykurs
+    config.app_generators.stylesheet_engine :less
+    config.assets.initialize_on_precompile = false
+
   end
 end
