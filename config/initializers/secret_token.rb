29da4d033b57a58e97cb0b2afafc4b5959d4b5c3
Dag Christoffersen <deejunit@gmail.com>
2012-02-01 09:16:27 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vegskade::Application.config.secret_token = '1fc1bdb7db0ac6c17f8fe0d008e2f9392e47b79441eaab4001bb87464ee1fe9b6fbb437fab4fb0cbc7d80113bc0ea09347b5a79d7e7a242f0a98755d17cc8c46'
